c6ef87358fda41f1945885a074693c22e372d613
F0xhopper <you@example.com>
2024-07-01 19:59:02 +0100

    slider&sellingFormatAdded

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ ChartJS.register(
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
+  const [sliderInput, setSliderInput] = useState("200");
   const [listings, setListings] = useState([]);
   const [averagePrice, setAveragePrice] = useState(null);
   const [chartData, setChartData] = useState({});
@@ -31,7 +32,9 @@ function App() {
   const [soldLastMonth, setSoldLastMonth] = useState(0);
   const [titleDisplayLength, setTitleDisplayLength] = useState(50); // Default length
   const [loading, setLoading] = useState(false);
-  const [priceChange, setPriceChange] = useState(null); // State for price change
+  const [priceRange, setPriceRange] = useState({ highest: 0, lowest: 0 });
+  const [averageSalesPerWeek, setAverageSalesPerWeek] = useState(0);
+  const [selectedFormat, setSelectedFormat] = useState("both");
 
   useEffect(() => {
     const handleResize = () => {
@@ -50,11 +53,20 @@ function App() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const fetchSoldListings = async (query) => {
+  const fetchSoldListings = async () => {
     try {
-      const response = await fetch(
-        `http://127.0.0.1:5000/scrape?item=${encodeURIComponent(searchInput)}`
-      );
+      let url = `http://127.0.0.1:5000/scrape?item=${encodeURIComponent(
+        searchInput
+      )}&limit=${sliderInput}`;
+      if (selectedFormat === "buy-it-now") {
+        url += "&format=buy-it-now";
+      } else if (selectedFormat === "auction") {
+        url += "&format=auction";
+      } else if (selectedFormat === "both") {
+        url += "&format=both";
+      }
+
+      const response = await fetch(url);
       if (!response.ok) {
         throw new Error("Failed to fetch data");
       }
@@ -88,55 +100,68 @@ function App() {
     );
     return total / listings.length;
   };
+  const calculatePriceRange = (listings) => {
+    const prices = listings.map((listing) => parseFloat(listing.price));
+    const highestPrice = Math.max(...prices);
+    const lowestPrice = Math.min(...prices);
+    return {
+      highest: highestPrice,
+      lowest: lowestPrice,
+    };
+  };
 
+  const calculateAverageSalesPerWeek = (listings) => {
+    if (listings.length === 0) return 0;
+
+    const firstSaleDate = new Date(listings[0].soldDate);
+    const lastSaleDate = new Date(listings[listings.length - 1].soldDate);
+    const weeksDiff = Math.abs(
+      (lastSaleDate - firstSaleDate) / (1000 * 60 * 60 * 24 * 7)
+    );
+
+    return (listings.length / weeksDiff).toFixed(2);
+  };
+  const handleClear = () => {
+    setSearchInput(""); // Reset search input
+    setSliderInput("200"); // Reset slider input
+
+    setChartData({}); // Clear chart data
+    setSoldLastMonth(0); // Reset sold last month
+    setAverageSalesPerWeek(0); // Reset average sales per week
+    setPriceRange({ highest: 0, lowest: 0 }); // Reset price range
+    setSelectedFormat("both"); // Reset selected format
+  };
   const handleSearch = async () => {
     setLoading(true);
     const soldListings = await fetchSoldListings(searchInput);
     setLoading(false);
     if (soldListings.length > 0) {
-      // Filter listings to only include those from the past month
       soldListings.sort((a, b) => new Date(a.soldDate) - new Date(b.soldDate));
-
       setListings(soldListings);
 
       const average = calculateAveragePrice(soldListings);
       setAveragePrice(average.toFixed(2));
 
-      // Calculate the number of items sold in the last month
       calculateSoldLastMonth(soldListings);
 
-      // Calculate the initial average price (first 10% of the listings)
-      const initialListings = soldListings.slice(
-        0,
-        Math.ceil(soldListings.length * 0.1)
-      );
-      const initialAverage = calculateAveragePrice(initialListings);
+      const averageSales = calculateAverageSalesPerWeek(soldListings);
+      setAverageSalesPerWeek(averageSales);
 
-      // Calculate the recent average price (last 10% of the listings)
-      const recentListings = soldListings.slice(
-        -Math.ceil(soldListings.length * 0.1)
-      );
-      const recentAverage = calculateAveragePrice(recentListings);
+      const range = calculatePriceRange(soldListings);
+      setPriceRange(range);
 
-      // Calculate the price change
-      const priceChange =
-        ((recentAverage - initialAverage) / initialAverage) * 100;
-      setPriceChange(priceChange.toFixed(2));
-
-      // Prepare data for the chart
       const dates = soldListings.map((item) =>
         new Date(item.soldDate).toLocaleDateString()
       );
       const prices = soldListings.map((item) => parseFloat(item.price));
 
-      // Calculate linear regression (trend line)
       const n = dates.length;
       let sumX = 0,
         sumY = 0,
         sumXY = 0,
         sumXX = 0;
       for (let i = 0; i < n; i++) {
-        sumX += i + 1; // x values are 1, 2, 3, ...
+        sumX += i + 1;
         sumY += prices[i];
         sumXX += (i + 1) * (i + 1);
         sumXY += (i + 1) * prices[i];
@@ -181,7 +206,7 @@ function App() {
       setAveragePrice(null);
       setChartData({});
       setSoldLastMonth(0);
-      setPriceChange(null);
+      setAverageSalesPerWeek(0);
     }
   };
 
@@ -216,28 +241,156 @@ function App() {
       >
         <img className="logoImage" src={logo} alt="Logo" />
       </div>
-      <div className="searchInputContainer">
-        <input
-          placeholder="Search item"
-          className="searchInput"
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
-        <button className="searchButton" onClick={handleSearch}>
-          Search
-        </button>
-      </div>
+      {averagePrice == null && (
+        <div className="searchMainContainer">
+          <div className="searchInputContainer">
+            <input
+              placeholder="Search item"
+              className="searchInput"
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </div>
+          <div className="searchOptionsContainer">
+            {" "}
+            <div className="sliderMainContainer">
+              <h1 className="sliderTitleText">
+                Maximum amount of listings:{" "}
+                <span className="sliderTitleTextNumber">{sliderInput}</span>
+              </h1>
+              <div className="sliderContainer">
+                <h1 className="sliderRangeText">1</h1>
+                <input
+                  onChange={(e) => {
+                    setSliderInput(e.target.value);
+                  }}
+                  type="range"
+                  min="1"
+                  max="250"
+                  value={sliderInput}
+                  className="slider"
+                ></input>{" "}
+                <h1 className="sliderRangeText">250</h1>
+              </div>
+            </div>
+            <div className="checkBoxContainer">
+              <label className="radioInput">
+                <input
+                  type="radio"
+                  checked={selectedFormat === "buy-it-now"}
+                  onChange={() => setSelectedFormat("buy-it-now")}
+                />
+                Buy It Now
+              </label>
+              <label className="radioInput">
+                <input
+                  type="radio"
+                  checked={selectedFormat === "auction"}
+                  onChange={() => setSelectedFormat("auction")}
+                />
+                Auction
+              </label>
+              <label className="radioInput">
+                <input
+                  type="radio"
+                  checked={selectedFormat === "both"}
+                  onChange={() => setSelectedFormat("both")}
+                />
+                Both
+              </label>
+            </div>
+          </div>{" "}
+          <div className="buttonsContainer">
+            <button className="clearButton" onClick={handleClear}>
+              Clear
+            </button>
+            <button className="searchButton" onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+        </div>
+      )}
       {loading && <div className="loadingAnimation">Loading...</div>}
-
       {averagePrice != null && (
         <div className="resultsContainer">
-          <div className="averagePriceContainer">
-            <h4 className="averagePriceIntroText">
-              The average price for '{searchInput}' based on the last{" "}
-              {listings.length} sold listings is:
-            </h4>
-            <h1 className="averagePriceNumberText">
-              £{averagePrice ? averagePrice : 0}
-            </h1>
+          <div className="searchAverageContainer">
+            <div className="searchMainContainer">
+              <div className="searchInputContainer">
+                <input
+                  placeholder="Search item"
+                  className="searchInput"
+                  onChange={(e) => setSearchInput(e.target.value)}
+                />
+              </div>
+              <div className="searchOptionsContainer">
+                {" "}
+                <div className="sliderMainContainer">
+                  <h1 className="sliderTitleText">
+                    Maximum amount of listings:{" "}
+                    <span className="sliderTitleTextNumber">{sliderInput}</span>
+                  </h1>
+                  <div className="sliderContainer">
+                    <h1 className="sliderRangeText">1</h1>
+                    <input
+                      onChange={(e) => {
+                        setSliderInput(e.target.value);
+                      }}
+                      type="range"
+                      min="1"
+                      max="250"
+                      value={sliderInput}
+                      className="slider"
+                    ></input>{" "}
+                    <h1 className="sliderRangeText">250</h1>
+                  </div>
+                </div>
+                <div className="checkBoxContainer">
+                  <label className="radioInput">
+                    <input
+                      className="radioInput"
+                      type="radio"
+                      checked={selectedFormat === "buy-it-now"}
+                      onChange={() => setSelectedFormat("buy-it-now")}
+                    />
+                    Buy It Now
+                  </label>
+                  <label className="radioInput">
+                    <input
+                      className="radioInput"
+                      type="radio"
+                      checked={selectedFormat === "auction"}
+                      onChange={() => setSelectedFormat("auction")}
+                    />
+                    Auction
+                  </label>
+                  <label className="radioInput">
+                    <input
+                      className="radioInput"
+                      type="radio"
+                      checked={selectedFormat === "both"}
+                      onChange={() => setSelectedFormat("both")}
+                    />
+                    Both
+                  </label>
+                </div>{" "}
+              </div>{" "}
+              <div className="buttonsContainer">
+                <button className="clearButton" onClick={handleClear}>
+                  Clear
+                </button>
+                <button className="searchButton" onClick={handleSearch}>
+                  Search
+                </button>
+              </div>
+            </div>
+            <div className="averagePriceContainer">
+              <h4 className="averagePriceIntroText">
+                The average price for '{searchInput}' based on the last{" "}
+                {listings.length} sold listings is:
+              </h4>
+              <h1 className="averagePriceNumberText">
+                £{averagePrice ? averagePrice : 0}
+              </h1>
+            </div>
           </div>
           <div className="chartContainer">
             <Line data={chartData} options={{ maintainAspectRatio: false }} />
@@ -265,9 +418,7 @@ function App() {
                       />
                       <div className="individualListingTextContainer">
                         <h4 className="individualListingTitleText">
-                          {listing.title.length > titleDisplayLength
-                            ? listing.title.slice(0, titleDisplayLength) + "..."
-                            : listing.title}
+                          {listing.title}
                         </h4>
                         <div>
                           <div className="individualListingPriceText">
@@ -288,20 +439,29 @@ function App() {
             </div>
             <div className="usefulStatsContainer">
               {soldLastMonth > 0 && (
-                <div className="soldLastMonthContainer">
-                  <h4>Total {searchInput} sales in the last month:</h4>
-                  <h1>{soldLastMonth}</h1>{" "}
-                </div>
-              )}
-              {priceChange != null && (
-                <div className="priceChangeContainer">
-                  <h4>
-                    Price change since{" "}
-                    {new Date(listings[0].soldDate).toLocaleDateString()}:
+                <div className="individualStatContainer">
+                  <h4 className="statTitleText">
+                    Total {searchInput} sales in the last month:
                   </h4>
-                  <h1>{priceChange}%</h1>
+                  <h1 className="statText">{soldLastMonth}</h1>
                 </div>
               )}
+              {averageSalesPerWeek > 0 && (
+                <div className="individualStatContainer">
+                  <h4 className="statTitleText">
+                    Average amount of {searchInput} sales per week:
+                  </h4>
+                  <h1 className="statText">{averageSalesPerWeek}</h1>
+                </div>
+              )}{" "}
+              <div className="individualStatContainer">
+                <h4 className="statTitleText">
+                  Price range for {searchInput}:
+                </h4>
+                <h1 className="statText">
+                  £{priceRange.lowest} - £{priceRange.highest}
+                </h1>
+              </div>
             </div>
           </div>
         </div>
